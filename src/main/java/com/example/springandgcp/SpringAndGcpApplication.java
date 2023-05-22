021f1071b8f78dd065545684358352dcf6786a59
willythedev <willy.mwangi@acrolinx.com>
2023-05-22 14:57:44 +0300

    Feature: add greeting controller

--- src/main/java/com/example/springandgcp/SpringAndGcpApplication.java
+++ src/main/java/com/example/springandgcp/SpringAndGcpApplication.java
@@ -2,6 +2,10 @@ package com.example.springandgcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringAndGcpApplication {
@@ -10,4 +14,12 @@ public class SpringAndGcpApplication {
 		SpringApplication.run(SpringAndGcpApplication.class, args);
 	}
 
+	@RequestMapping("/api/v1/")
+	@RestController
+	public class GreetingsController {
+		@GetMapping("/greet")
+		public ResponseEntity<String> greet() {
+			return ResponseEntity.ok().body("Hello there!");
+		}
+	}
 }
